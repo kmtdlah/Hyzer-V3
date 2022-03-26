d73a7000c97627fde64f97974fde18b8c38a29e8
kmtdlah <99109389+kmtdlah@users.noreply.github.com>
2022-03-26 15:14:43 +0700

    Update viadana.js

--- plugins/viadana.js
+++ plugins/viadana.js
@@ -1,11 +1,8 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 ╠═〘 PEMBAYARAN 〙 ═
-╠═ Untuk Pembayaran Via Gopay
-╠➥ *6287892711054*
-║- Owner Bot:
-║- wa.me/6287892711054
-╠═〘 Hyzer 〙 ═
+╠═ -
+╠═〘 F 〙 ═
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
